4587e55ad7eaad74c52b34aedcc8320e153d4a52
Esteban <estebanagonzalez@outlook.es>
2023-10-31 10:58:01 -0300

    Segundo Commit - Implementación mediaQueries

--- main.js
+++ main.js
@@ -1,54 +1,46 @@
-// Obtén una referencia al elemento del logo por su id
 const logo = document.getElementById('logo');
 
-// Agrega un evento 'mouseenter' (mouse entra) para aplicar el zoom
 logo.addEventListener('mouseenter', () => {
-  logo.style.transform = 'scale(1.1)'; // Ajusta el valor para controlar el nivel de zoom
-  logo.style.transition = 'transform 0.3s'; // Agrega una transición suave
+  logo.style.transform = 'scale(1.1)';
+  logo.style.transition = 'transform 0.3s';
 });
 
-// Agrega un evento 'mouseleave' (mouse sale) para revertir el zoom
 logo.addEventListener('mouseleave', () => {
-  logo.style.transform = 'scale(1)'; // Restaura el tamaño original
+  logo.style.transform = 'scale(1)';
 });
-// Obtén una referencia a todos los elementos con la clase "zoom-effect"
 const infoBoxes = document.querySelectorAll('.zoom-effect');
 
-// Itera sobre cada "info-box"
 infoBoxes.forEach(infoBox => {
-  // Agrega un evento 'mouseenter' (mouse entra) para aplicar el zoom
   infoBox.addEventListener('mouseenter', () => {
-    infoBox.style.transform = 'scale(1.1)'; // Ajusta el valor para controlar el nivel de zoom
-    infoBox.style.transition = 'transform 0.3s'; // Agrega una transición suave
+    infoBox.style.transform = 'scale(1.1)';
+    infoBox.style.transition = 'transform 0.3s';
   });
 
-  // Agrega un evento 'mouseleave' (mouse sale) para revertir el zoom
   infoBox.addEventListener('mouseleave', () => {
-    infoBox.style.transform = 'scale(1)'; // Restaura el tamaño original
+    infoBox.style.transform = 'scale(1)';
   });
 });
 document.addEventListener('DOMContentLoaded', function () {
-    // Obtén una referencia al formulario por su ID
-    const formulario = document.getElementById('miFormulario');
-  
-    // Agrega un evento para manejar el envío del formulario
-    formulario.addEventListener('submit', function (event) {
-      event.preventDefault(); // Evita que el formulario se envíe automáticamente
-  
-      // Aquí puedes agregar código adicional para validar los datos del formulario si es necesario
-  
-      // Muestra una alerta al usuario
-      alert('Los datos han sido enviados al centro de contacto.');
-    });
+
+  const formulario = document.getElementById('miFormulario');
+
+
+  formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+
+
+
+    alert('Los datos han sido enviados al centro de contacto.');
   });
-  document.addEventListener('DOMContentLoaded', function () {
-    // Obtén una referencia al elemento de texto por su ID o clase
-    const numeroTelefono = document.getElementById('numero-telefono');
-  
-    // Agrega un evento para manejar el click en el elemento de texto
-    numeroTelefono.addEventListener('click', function () {
-      alert('Horarios de atención: Lun a Vie 09:00 - 18:00\n0800-333-4444 ó 0800-444-555');
-    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+
+  const numeroTelefono = document.getElementById('numero-telefono');
+
+
+  numeroTelefono.addEventListener('click', function () {
+    alert('Horarios de atención: Lun a Vie 09:00 - 18:00\n0800-333-4444 ó 0800-444-555');
   });
-  
-  
+});
+
